c0a37e1e140d9e38878cd04fe5c9f91c501b92c4
Héctor Flores <frhectorin@gmail.com>
2025-05-28 00:15:20 +0200

    Update postinstall.sh

--- .devcontainer/extras/postinstall.sh
+++ .devcontainer/extras/postinstall.sh
@@ -27,18 +27,6 @@ cd ..
 cd DynReActService
 poetry lock && poetry install
 
-cat <<EOF > .env
-# Environment Variables
-CONFIG_PROVIDER=default+file:./data/config/site.json
-COST_PROVIDER=ras:costs
-PYTHONUNBUFFERED=1
-SHORT_TERM_PLANNING_PARAMS=ras+file:./data/config/stp_context.json
-SNAPSHOT_PROVIDER=ras+file:./data/snapshots
-STP_FRONTEND=dynreact.stp_gui_ras.agentPageRas
-TOPIC_CALLBACK=Dyn-Callback
-TOPIC_GEN=Dyn-Gen
-EOF
-
 #######
 # Config ZSH
 #######

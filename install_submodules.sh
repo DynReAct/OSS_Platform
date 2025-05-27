3bb08e19dff9b1155a6c1e85beca5cfe4d8f476a
Héctor Flores <frhectorin@gmail.com>
2025-05-28 00:15:33 +0200

    Update install_submodules.sh

--- install_submodules.sh
+++ install_submodules.sh
@@ -14,3 +14,15 @@ cd ..
 cd LongTermPlanning
 poetry lock && poetry install
 cd ../..
+
+cat <<EOF > .env
+# Environment Variables
+CONFIG_PROVIDER=default+file:./data/config/site.json
+COST_PROVIDER=ras:costs
+PYTHONUNBUFFERED=1
+SHORT_TERM_PLANNING_PARAMS=ras+file:./data/config/stp_context.json
+SNAPSHOT_PROVIDER=ras+file:./data/snapshots
+STP_FRONTEND=dynreact.stp_gui_ras.agentPageRas
+TOPIC_CALLBACK=Dyn-Callback
+TOPIC_GEN=Dyn-Gen
+EOF

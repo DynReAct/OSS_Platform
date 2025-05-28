e138276790fde91bd2aff6a5e42ea5a71148addb
Hector Flores <frhectorin@gmail.com>
2025-05-28 00:10:16 +0000

    Added submodule install

--- install_submodules.sh
+++ install_submodules.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR
+
+# Get Poetry's env path for oss-plataform
+if [[ -n "$VIRTUAL_ENV_PROMPT" && "$VIRTUAL_ENV_PROMPT" == *oss-plataform* ]] && [[ -n "$VIRTUAL_ENV" && "$VIRTUAL_ENV" == *oss-plataform* ]]; then
+    POETRY_ENV_PATH=$(poetry env list --full-path | grep oss-plataform | grep Activated | awk '{print $1}')
+    echo "Poetry environment 'oss-plataform' is already activated: ${POETRY_ENV_PATH:-$VIRTUAL_ENV}"
+else
+    echo "Activating Poetry environment 'oss-plataform'..."
+    POETRY_ENV_PATH=$(poetry env list --full-path | grep oss-plataform | awk '{print $1}')
+    if [[ -z "$POETRY_ENV_PATH" ]]; then
+        echo "Error: No Poetry environment named 'oss-plataform' found."
+        exit 1
+    fi
+    source "$POETRY_ENV_PATH/bin/activate"
+    echo "Activated: $(which python)"
+fi
+
 git submodule update --init --recursive
 cd DynReActService
 
@@ -13,7 +32,7 @@ cd ..
 
 cd LongTermPlanning
 poetry lock && poetry install
-cd ../..
+cd ..
 
 cat <<EOF > .env
 # Environment Variables
@@ -23,6 +42,7 @@ PYTHONUNBUFFERED=1
 SHORT_TERM_PLANNING_PARAMS=ras+file:./data/config/stp_context.json
 SNAPSHOT_PROVIDER=ras+file:./data/snapshots
 STP_FRONTEND=dynreact.stp_gui_ras.agentPageRas
-TOPIC_CALLBACK=Dyn-Callback
-TOPIC_GEN=Dyn-Gen
 EOF
+
+poetry lock && poetry install --extras "ras"
+cd $SCRIPT_DIR

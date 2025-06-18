e7f326e6aae946f3063312614223b40bf9e24581
Hector Flores <frhectorin@gmail.com>
2025-06-18 15:48:56 +0200

    Swap scripts

--- .devcontainer/extras/postCreate.sh
+++ .devcontainer/extras/postCreate.sh
@@ -1,51 +1,19 @@
 #!/bin/bash
 
-#######
-# Basic Config
-#######
+WORKSPACE="/workspace"
+git config --global --add safe.directory "$WORKSPACE"
 
-sudo chown root:docker /var/run/docker.sock
-sudo service socat-forwarding reload 
-cd /workspace
+LongTermPlanning="$WORKSPACE/DynReActService/LongTermPlanning"
+if [ "$(ls -A $LongTermPlanning)" ]; then
+    git config --global --add safe.directory "$LongTermPlanning"
+fi
 
-#######
-# Poetry
-#######
+LotCreationRas="$WORKSPACE/DynReActService/LotCreationRas"
+if [ "$(ls -A $LotCreationRas)" ]; then
+    git config --global --add safe.directory "$LotCreationRas"
+fi
 
-poetry init --no-interaction --name OSS_Plataform --python "^3.12"
-poetry install --no-root
-eval $(poetry env activate)
-
-cd DynReActBase
-poetry lock && poetry install
-cd ..
-
-cd MidTermPlanning
-poetry lock && poetry install
-cd ..
-
-cd SampleUseCase
-poetry lock && poetry install
-cd ..
-
-cd ShortTermPlanning
-poetry lock && poetry install
-cd ..
-
-cd DynReActService
-poetry lock && poetry install
-cd /workspace
-
-#######
-# Config ZSH
-#######
-# powerline fonts for zsh theme
-git clone https://github.com/powerline/fonts.git
-cd fonts
-./install.sh
-cd .. && rm -rf fonts
-
-# oh-my-zsh plugins
-zsh -c 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k'
-cp .devcontainer/extras/.zshrc ~
-cp .devcontainer/extras/.p10k.zsh ~
+ShortTermRAS="$WORKSPACE/DynReActService/ShortTermRAS"
+if [ "$(ls -A $ShortTermRAS)" ]; then
+    git config --global --add safe.directory "$ShortTermRAS"
+fi
---
+++ .devcontainer/extras/postInstall.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#######
+# Basic Config
+#######
+
+sudo chown root:docker /var/run/docker.sock
+sudo service socat-forwarding reload 
+cd /workspace
+
+#######
+# Poetry
+#######
+
+poetry init --no-interaction --name OSS_Plataform --python "^3.12"
+poetry install --no-root
+eval $(poetry env activate)
+
+cd DynReActBase
+poetry lock && poetry install
+cd ..
+
+cd MidTermPlanning
+poetry lock && poetry install
+cd ..
+
+cd SampleUseCase
+poetry lock && poetry install
+cd ..
+
+cd ShortTermPlanning
+poetry lock && poetry install
+cd ..
+
+cd DynReActService
+poetry lock && poetry install
+cd /workspace
+
+#######
+# Config ZSH
+#######
+# powerline fonts for zsh theme
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd .. && rm -rf fonts
+
+# oh-my-zsh plugins
+zsh -c 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k'
+cp .devcontainer/extras/.zshrc ~
+cp .devcontainer/extras/.p10k.zsh ~
